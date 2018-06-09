2e5af929fb5beabfc9135ba310bfc105bc19bfbd
bhavesh <bhaveshbangera@gmail.com>
2018-06-09 21:26:52 +0530

    Created our own API using Django-REST Framework

---
+++ languages/views.py
@@ -0,0 +1,21 @@
+from django.shortcuts import render
+from rest_framework import viewsets, permissions
+from .models import Paradigm, Language, Programmer
+from .serializers import ParadigmSerializer, LanguageSerializer, ProgrammerSerializer
+
+# Create your views here.
+class ParadigmView(viewsets.ModelViewSet):
+	queryset = Paradigm.objects.all()
+	serializer_class = ParadigmSerializer
+    #permission_classes = (permissions.IsAuthenticatedOrReadOnly)
+
+class LanguageView(viewsets.ModelViewSet):
+	queryset = Language.objects.all()
+	serializer_class = LanguageSerializer
+	#permission_classes = (permissions.IsAuthenticatedOrReadOnly,)
+
+
+class ProgrammerView(viewsets.ModelViewSet):
+	queryset = Programmer.objects.all()
+	serializer_class = ProgrammerSerializer
+	#permission_classes = (permissions.IsAuthenticatedOrReadOnly,)
